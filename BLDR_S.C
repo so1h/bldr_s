21bd41479bd4af091db82f838850725399479f07
Pedro Pablo López Rodríguez <plopez@etsisi.upm.es>
2019-02-12 23:18:02 +0100

    Add files via upload

--- BLDR_S.C
+++ BLDR_S.C
@@ -84,15 +84,15 @@ int printfBIOS ( char * fmt, ... )
 {
     va_list ap ;
     char buf [ 512 ] ;
-	if (strchr(fmt, '%') == NULL) 
+    if (strchr(fmt, '%') == NULL) 
         printstrBIOS(fmt) ;
-	else
-	{ 
+    else
+    { 
         va_start(ap, fmt) ;
         vsprintf(buf, fmt, ap) ;
         va_end(ap) ;
         printstrBIOS(buf) ;
-    }    	
+    }       
 }
 
 void __setup_unreal ( void ) ;     /* to reenable unreal mode if necessary */
